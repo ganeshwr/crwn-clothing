fb916d98ac3faf1e918d5e0c8ff48493d6068c62
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-12 11:10:25 +0700

    Add Navigation & Sign In menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<h1>SHOP PAGE</h1>} />
+          <Route path="sign-in" element={<SignIn/>} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,7 @@
 import Directory from "../../components/directory/directory.component";
 
+import { Outlet } from "react-router-dom";
+
 const Home = () => {
   const categories = [
     {
@@ -31,6 +33,7 @@ const Home = () => {
 
   return (
     <div>
+      <Outlet />
       <Directory categories={categories} />
     </div>
   );
