37c1e7e4b8de08bae4584cd1403e0bc2fe238614
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-16 16:01:37 +0700

    rename sign-in to Authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -3,7 +3,7 @@ import "./sign-in.styles.scss"
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
   return (
     <div className="forms-container">
       <SignInForm />
@@ -12,4 +12,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
