9a13f68514f2db21e162450586a6d61c3e9f8484
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-21 16:09:42 +0700

    Add cart dropdown + implement it

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -8,14 +8,14 @@
 import "./button.styles.scss"
 
 const BUTTON_TYPE_CLASSES = {
-  google: "google-sign-in",
-  inverted: "inverted",
+  google: " google-sign-in",
+  inverted: " inverted",
 };
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
     <button
-      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      className={`button-container${BUTTON_TYPE_CLASSES[buttonType] || ''}`}
       {...otherProps}
     >
       {children}
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,26 @@
 import { Outlet, Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+  const [openCart, setOpenCart] = useState(false)
+  
+  const toggleCart = () => {
+    console.log("LOL");
+    setOpenCart(!openCart)
+    console.log(openCart);
+  }
+  
   return (
     <>
       <div className="navigation">
@@ -30,7 +40,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon onClick={toggleCart}/>
         </div>
+        {openCart && <CartDropdown />}
       </div>
       <Outlet />
     </>
