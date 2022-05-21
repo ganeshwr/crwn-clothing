3e5a270e7ecc30bac076c4d958c816ed83473641
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-21 15:03:03 +0700

    Add Product context

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Products from "./routes/products/products.component";
 
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 const App = () => {
   return (
     <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Home />} />
-            <Route path="shop" element={<h1>SHOP PAGE</h1>} />
-            <Route path="sign-in" element={<Authentication />} />
-          </Route>
-        </Routes>
-      </Router>
+      <ProductsProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route index element={<Home />} />
+              <Route path="shop" element={<Products />} />
+              <Route path="sign-in" element={<Authentication />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ProductsProvider>
     </UserProvider>
   );
 };
