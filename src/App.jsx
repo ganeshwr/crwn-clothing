81c8dfedef7b48e225b6b782d34a5fcb32e78cf9
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-21 15:43:54 +0700

    Add Shop product card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-import Products from "./routes/products/products.component";
+import Shop from "./routes/shop/shop.component";
 
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
@@ -16,7 +16,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Navigation />}>
               <Route index element={<Home />} />
-              <Route path="shop" element={<Products />} />
+              <Route path="shop" element={<Shop />} />
               <Route path="sign-in" element={<Authentication />} />
             </Route>
           </Routes>
