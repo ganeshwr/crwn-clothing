59e2c690677c2fec6b7ffe835e6b5dd4287b1c95
Ganesha Wahyudi Rawlins <ganeshwr7@gmail.com>
2022-05-25 14:00:38 +0700

    Convert user context's state to reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
@@ -11,9 +11,38 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const UserReducer = (state, action) => {
+  const { type, payload } = action; //type is guaranteed, but payload is optional
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled action type ${type}`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  const [state, dispatch] = useReducer(UserReducer, INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user,
+    });
+  };
+  // const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser: state.currentUser, setCurrentUser };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(async (user) => {
